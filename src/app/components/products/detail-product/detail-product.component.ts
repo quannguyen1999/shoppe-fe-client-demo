8972017e2435d4178c8fff6adb2716a5e65d5b8e
quannguyen1999 <nguyendanganhquan99@gmail.com>
2024-03-17 23:10:14 +0700

    fix bug

--- src/app/components/products/detail-product/detail-product.component.ts
+++ src/app/components/products/detail-product/detail-product.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
+import { DEFAULT_PRODUCT_COLUMNS } from 'src/app/constants/constant-value-model';
 import { imageDataFakeOne, imageDataFakeTwo } from 'src/app/constants/data-fake.model';
 import { ImageCommon } from 'src/app/models/image-common.model';
+import { Product, ProductRequestModel } from 'src/app/models/product.model';
 import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
+import { ToastrService } from 'src/app/services/toastr.service';
 
 @Component({
   selector: 'app-detail-product',
@@ -12,6 +15,22 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./detail-product.component.scss']
 })
 export class DetailProductComponent implements OnInit {
+  productRequestModel: ProductRequestModel = {
+    id: '',
+    name: '',
+    image: '',
+    quantity: 0,
+    price: 0,
+    discount: 0,
+    idCategory: '',
+    createFromDate: null,
+    createToDate: null,
+    listSorted: null,
+    listFields: DEFAULT_PRODUCT_COLUMNS,
+    isGetTopProduct: false,
+    isSuggestProduct: false
+  };
+  
   cities!: string[];
 
   images: ImageCommon[] | undefined;
@@ -22,6 +41,16 @@ export class DetailProductComponent implements OnInit {
   currentImage: string = '';
 
   numberOfItem: number = 1;
+
+  product: Product = {
+    id: 0,
+    name: '',
+    image: '',
+    quantity: 0,
+    price: 0,
+    discount: 0,
+    description: ''
+  };
 
   responsiveOptions: any[] = [
       {
@@ -41,26 +70,44 @@ export class DetailProductComponent implements OnInit {
   constructor(private router: Router,
             private cartService: CartService,
             private messageService: MessageService,
-            private productService: ProductService
+            private productService: ProductService,
+            private routerActive: ActivatedRoute,
+            private toastrService: ToastrService
     ){
-    
+     this.productRequestModel.id = this.routerActive.snapshot.params['id'];
+     this.productService.getListProduct(0, 1, DEFAULT_PRODUCT_COLUMNS, this.productRequestModel).subscribe(
+      (data)=>{
+        this.product = data.data[0];
+        this.listImageFake = [this.product.image];
+        this.images = [{id: 1, type:'image', image: this.product.image},
+    {id: 2, type:'video', image: this.product.image},
+    {id: 3, type:'image', image: this.product.image},
+    {id: 4, type:'image', image: this.product.image},
+    {id: 5, type:'image', image: this.product.image},
+    {id: 6, type:'image', image: this.product.image}];
+        this.cities = ['Hà nội', 'Hồ Chí Minh']
+
+        this.imageDataFakeOne = imageDataFakeOne;
+
+        this.currentImage = imageDataFakeOne;
+      },
+      (error)=>{
+        toastrService.getPopUpErrorTypeString("Internal Server Error");
+      }
+     )
   }
 
   ngOnInit() {
-    this.images = [{id: 1, type:'image', image: imageDataFakeOne},
-    {id: 2, type:'video', image: imageDataFakeTwo},
-    {id: 3, type:'image', image: imageDataFakeOne},
-    {id: 4, type:'image', image: imageDataFakeTwo},
-    {id: 5, type:'image', image: imageDataFakeOne},
-    {id: 6, type:'image', image: imageDataFakeTwo}];
+    // this.images = [{id: 1, type:'image', image: imageDataFakeOne},
+    // {id: 2, type:'video', image: imageDataFakeTwo},
+    // {id: 3, type:'image', image: imageDataFakeOne},
+    // {id: 4, type:'image', image: imageDataFakeTwo},
+    // {id: 5, type:'image', image: imageDataFakeOne},
+    // {id: 6, type:'image', image: imageDataFakeTwo}];
 
-    this.listImageFake = [imageDataFakeOne, imageDataFakeTwo, imageDataFakeOne, imageDataFakeTwo]
+    // this.listImageFake = [imageDataFakeOne, imageDataFakeTwo, imageDataFakeOne, imageDataFakeTwo]
 
-    this.cities = ['Hà nội', 'Hồ Chí Minh']
-
-    this.imageDataFakeOne = imageDataFakeOne;
-
-    this.currentImage = imageDataFakeOne;
+    
   }
 
   testClick(image: string){
