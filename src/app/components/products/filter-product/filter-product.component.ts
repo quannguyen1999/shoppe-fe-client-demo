0d87e4ea707fa997cc4a0ab9361b83ab37ceb07e
quannguyen1999 <nguyendanganhquan99@gmail.com>
2024-04-03 22:31:15 +0700

    remove file specc

---
+++ src/app/components/products/filter-product/filter-product.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-product',
+  templateUrl: './filter-product.component.html',
+  styleUrls: ['./filter-product.component.scss']
+})
+export class FilterProductComponent {
+
+}
